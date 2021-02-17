840271ebc43bdda68bde6fb4bdc726e39bddd86f
freejob985 <freejob985@gmail.com>
2021-02-18 01:03:25 +0200

    asd

--- routes/web.php
+++ routes/web.php
@@ -373,7 +373,7 @@ Route::group(['prefix'=>'admin', 'middleware' => ['auth', 'admin'] ], function()
  */
 //Route::get('{slug}', 'PostController@singlePage')->name('page');
 
-Route::get('blog', 'NewsController@blog')->name('News');
+Route::get('News/post', 'NewsController@blog')->name('News');
 Route::get('{slug}', 'PostController@postSingle')->name('post');
 Route::get('post/{id?}', 'PostController@postProxy')->name('post_proxy');
 
