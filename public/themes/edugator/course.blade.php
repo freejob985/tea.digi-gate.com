70c5aad88deed5418299270831f8c96c614fae6b
freejob985 <freejob985@gmail.com>
2021-02-18 01:38:40 +0200

    asd

--- public/themes/edugator/course.blade.php
+++ public/themes/edugator/course.blade.php
@@ -35,7 +35,19 @@
                 </div>
 
                 <div class="col-md-4">
-
+                    <div class="course-card-img-wrap">
+                        <a href="{{route('course', $course->slug)}}">
+                            <img src="{{$course->thumbnail_url}}" class="img-fluid" />
+                        </a>
+            
+                        <button class="course-card-add-wish btn btn-link btn-sm p-0" data-course-id="{{$course->id}}">
+                            @if($auth_user && in_array($course->id, $auth_user->get_option('wishlists', []) ))
+                                <i class="la la-heart"></i>
+                            @else
+                                <i class="la la-heart-o"></i>
+                            @endif
+                        </button>
+                    </div>
                     <div class="page-header-right-enroll-box p-3 mt-sm-4 mt-md-0 bg-white shadow">
 
                         @if( $isEnrolled)
