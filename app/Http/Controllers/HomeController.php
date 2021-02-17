26b7ec3968615b3efc9df49ca983624437525d00
freejob985 <freejob985@gmail.com>
2021-02-17 23:57:12 +0200

    asd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
         $featured_courses = Course::publish()->whereIsFeatured(1)->orderBy('featured_at', 'desc')->take(6)->get();
         $popular_courses = Course::publish()->whereIsPopular(1)->orderBy('popular_added_at', 'desc')->take(8)->get();
         $posts = Post::post()->publish()->take(3)->get();
-
+dd(theme('index'));
         return view(theme('index'), compact('title', 'new_courses', 'featured_courses', 'popular_courses', 'posts'));
     }
 
